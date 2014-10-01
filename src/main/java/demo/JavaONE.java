1806edcfdde2318395be111b7c74602446fd54c6
Christian Edward Gruber <cgruber@google.com>
2014-10-01 10:43:31 -0700

    Add in the fun bits.

--- src/main/java/demo/JavaONE.java
+++ src/main/java/demo/JavaONE.java
@@ -1,22 +1,38 @@
 package demo;
 
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
 import dagger.Component;
+import dagger.Module;
+import dagger.Provides;
 import javax.inject.Inject;
 
 public class JavaONE {
 
-  @Component
+  @Component(modules = MessageModule.class)
   interface Server {
-    MailService service();
+    MailService mail();
   }
 
   static class MailService {
-    @Inject public MailService() {
+    private final Supplier<String> message;
+    @Inject public MailService(Supplier<String> message) {
+      this.message = message;
+    }
 
+    void send() {
+      System.out.println("Mail sent: " + message);
+    }
+  }
+
+  @Module static class MessageModule {
+    @Provides Supplier<String> message() {
+      return Suppliers.ofInstance("Foo");
     }
   }
 
   public static void main(String ... args) {
-
+    Server server = Dagger_JavaONE$Server.create();
+    server.mail();
   }
 }
